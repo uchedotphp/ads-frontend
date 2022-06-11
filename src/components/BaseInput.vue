2cbaa47572fa9596bc3756dcbc1644caf2de08c5
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2022-06-11 16:08:23 +0100

    [feat] Create a base input component.

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" class="base-input" :placeholder="placeHolder" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    placeHolder: {
+      type: String,
+      default: "Email",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.base-input {
+  width: 100%;
+  border-radius: 12px;
+  padding: 10px 15px;
+  font-size: 20px;
+  outline: none;
+  border: none;
+}
+</style>
