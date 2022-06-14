37e4308bb4128ad8c48b95f1a22329fd83b3b1ed
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2022-06-14 23:01:56 +0100

    [styles] Update to TopHeader component.

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="top-header d-sm-flex row align-items-center">
-    <div class="logo-area d-none col-3 col-lg-2 d-sm-flex align-items-center">
+    <div class="logo-area d-none col-auto d-sm-flex align-items-center">
       <img
         src="../assets/images/poptin-logo.png"
         height="40"
@@ -8,9 +8,13 @@
       />
       <h2 class="title ms-2">Poptin test</h2>
     </div>
-    <div class="col me-auto">
-      <button @click="save" class="btn save-template">
-        <span v-if="!loading" class="fw-bold"> Save template </span>
+    <div class="col d-flex align-items-center justify-content-center">
+      <i role="button" class="bi bi-arrow-clockwise text-danger me-3" style="font-size: 2rem;"></i>
+      <button @click="save" class="btn btn-outline-primary save-template">
+        <template v-if="!loading">
+          <i style="font-size: 1rem" class="bi bi-cloud-slash"></i>
+          <span class="fw-bold"> Unsaved changes </span>
+        </template>
         <LoadingSpinner size="sm" v-else> Saving template... </LoadingSpinner>
       </button>
       <button
@@ -19,17 +23,25 @@
         @click="previewTemplate"
         class="preview-link btn"
       >
-        Preview <span class="d-none d-sm-inline"> in browser </span>
+        Preview
       </button>
     </div>
     <div class="col-auto">
-      <a
+      <!-- <a
         href="https://github.com/uchedotphp/poptin-frontend"
         target="_blank"
         type="button"
         class="btn btn-outline-primary"
         >Github Repo</a
+      > -->
+      <button
+        disabled
+        type="button"
+        class="col-auto btn btn-outline-success"
       >
+        <i class="bi bi-back me-1"></i>
+        Copy script
+      </button>
     </div>
   </header>
 </template>
@@ -75,7 +87,7 @@ export default {
 <style lang="scss" scoped>
 .top-header {
   background-color: white;
-  padding: 20px 50px;
+  padding: 20px 10px;
   box-shadow: 0px 15px 23px rgba(208, 210, 218, 0.6);
 
   @media (max-width: 767.98px) {
@@ -84,16 +96,13 @@ export default {
 
   .logo-area {
     .title {
-      font-size: 18px;
+      font-size: 16px;
     }
   }
   .save-template {
-    background: linear-gradient(0deg, #6a11cb, #2575fc) !important;
+    // background: linear-gradient(0deg, #6a11cb, #2575fc) !important;
     width: fit-content;
     padding-inline: 20px;
-    color: #ffffff;
-    outline: none;
-    border: none;
   }
   .preview-link {
     margin-left: 30px;
