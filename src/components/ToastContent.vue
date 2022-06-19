13acc48c409c0bc3760048550c4f4f9fcb697632
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2022-06-19 16:58:17 +0100

    [chores] Update to drag and cancel handles

---
+++ src/components/ToastContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <strong class="me-auto">Bootstrap</strong>
+      <small class="text-muted">11 mins ago</small>
+      <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="toast"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="toast-body">Hello, world! This is a toast message.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToastContent",
+};
+</script>
+
+<style lang="scss" scoped></style>
